1ca8a7b0a11d61f2d21a2038a760f0daccc05977
Wosten01 <mribloko@gmail.com>
2023-03-04 01:09:12 +0400

    Add time

--- LAB1BUBLESORT/buble_sort.cpp
+++ LAB1BUBLESORT/buble_sort.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <time.h>
+#include <thread>
+#include <chrono>
+
 
 using namespace std;
 
 void oddEvenSorting(vector<int>& vec) {
 	int n = vec.size();
 	for (size_t i = 0; i < n; i++) {
+		// if (i % 1000 == 0){
+		// 	cout << "I'm alive" << "\n";
+		// }
 		for (size_t j = (i % 2) ? 0 : 1; j + 1 < n; j += 2) {
 			if (vec[j] > vec[j + 1]) {
 				swap(vec[j], vec[j + 1]);
@@ -14,25 +21,31 @@ void oddEvenSorting(vector<int>& vec) {
 	}
 }
 
-int main()
-{
-
-	int n = 100;
-
+void intiliazation(vector<int>& vec, int n){
 	// srand(time(NULL));
 
-	vector<int> a; 
-
 	for (int i = 0; i < n; i++)
 	{
-		a.push_back(rand());
+		vec.push_back(rand());
 	}
+}
 
-	oddEvenSorting(a);
-
-	for (int i = 0; i < n; i++)
+void printVector(vector<int> vec){
+	for (int i = 0; i < vec.size(); i++)
 	{
-		cout << a[i] << "\n";
+		cout << vec[i] << "\n";
 	}
-	
+}
+
+const int n = 50000;
+
+int main()
+{	
+	vector<int> vec; 
+	intiliazation(vec, n);
+
+	clock_t begin = clock();
+	oddEvenSorting(vec);
+    clock_t end = clock();
+    printf("Sort time %f for %d elements", (double)(end - begin) / CLOCKS_PER_SEC, n);
 }
