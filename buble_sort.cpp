95143513745ff3a099c418e236a561d8e613b5d9
Wosten01 <mribloko@gmail.com>
2023-03-03 23:14:33 +0400

    1

---
+++ buble_sort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T, size_t N >
+void oddEvenSorting(T (&array)[N]) {
+	for (size_t i = 0; i < N; i++) {
+	    // (i % 2) ? 0 : 1 возвращает 1, если i четное, 0, если i не четное
+		for (size_t j = (i % 2) ? 0 : 1; j + 1 < N; j += 2) {
+			if (array[j] > array[j + 1]) {
+				std::swap(array[j], array[j + 1]);
+			}
+		}
+	}
+}
+
+int main()
+{
+	cout << "Helloo";
+}
