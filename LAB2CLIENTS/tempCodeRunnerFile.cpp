0ee2ff2c6abe3207a39d72b0fe0562cea168078f
Wosten01 <mribloko@gmail.com>
2023-05-27 00:23:45 +0400

    update Customer

--- LAB2CLIENTS/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-    int initialNumber = 10;
-    // Конфигурационные данные 
-    TicketingMachine* ticketsMachine = new TicketingMachine(initialNumber);
-    Logger* logger = new Logger;
-    Customer* cust1 = new Customer;
